107527c09b811b08e35b06b1d48bea481653d527
TheForgem4ster <maxim.thudercat@gmail.com>
2023-02-06 19:14:15 +0200

    Final BackEnd

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ const start = async () => {
     try {
         const PORT = process.env.PORT || 5000;
         const app = await NestFactory.create(AppModule);
-
+        app.enableCors()
         await app.listen(PORT, () => console.log(`server start on PORT ${PORT}`));
     } catch (e) {
         console.log(e);
