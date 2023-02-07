b0954aa09599e308767dad7660232884e93362b2
TheForgem4ster <maxim.thudercat@gmail.com>
2023-02-07 18:18:02 +0200

    add file Create

--- client/src/pages/tracks/create.tsx
+++ client/src/pages/tracks/create.tsx
@@ -1,12 +1,25 @@
+import { Button } from "@mui/material";
 import StepWrapper from "components/StepWrapper";
 import MainLayouts from "layouts/MainLayouts";
-import React from "react";
+import React, { useState } from "react";
 
 const Create = () =>{
+
+    const [activeStep, setActiveStep] = useState(0);
+    const next = () => {
+        setActiveStep(prev => prev + 1);
+    }
+
+    const back =()=> {
+        setActiveStep(prev => prev - 1);
+    }
+
     return (
         <MainLayouts>
-            <StepWrapper activeStep={1}>
+            <StepWrapper activeStep={activeStep}>
                 <h1>Download Track</h1>
+                <Button onClick={back}>Back</Button>
+                <Button onClick={next}>Next</Button>
             </StepWrapper>
         </MainLayouts>
     )
