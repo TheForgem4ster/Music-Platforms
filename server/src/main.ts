d68e6f79835d7fd8e6c76b40d3b6936cae396b5d
TheForgem4ster <maxim.thudercat@gmail.com>
2023-03-06 16:19:16 +0200

    Add Swagger

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,12 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
 
 const start = async () => {
     try {
-
         const PORT = process.env.PORT || 5000;
         const app = await NestFactory.create(AppModule);
         app.enableCors()
+
+        const config = new DocumentBuilder().setTitle("musicPlatform").setDescription("full API")
+            .setVersion("1.0").addTag("API").build();
+        const document = SwaggerModule.createDocument(app, config);
+        SwaggerModule.setup("api", app, document);
+
         await app.listen(PORT, () => console.log(`server start on PORT ${PORT}`));
     } catch (e) {
         console.log(e);
